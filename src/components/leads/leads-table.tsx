dd3a52683264ae6adf51704e9aa86ce13b8b52fc
William Flowers <team@dillyos.com>
2025-12-19 07:42:00 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@r

--- src/components/leads/leads-table.tsx
+++ src/components/leads/leads-table.tsx
@@ -2,10 +2,10 @@
 
 import * as React from 'react';
 import {
-  CaretSortIcon,
-  ChevronDownIcon,
-  DotsHorizontalIcon,
-} from '@radix-ui/react-icons';
+  ArrowUpDown,
+  ChevronDown,
+  MoreHorizontal,
+} from 'lucide-react';
 import {
   ColumnDef,
   ColumnFiltersState,
@@ -71,7 +71,7 @@ export const columns: ColumnDef<Lead>[] = [
           onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
         >
           Prospect
-          <CaretSortIcon className="ml-2 h-4 w-4" />
+          <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       );
     },
@@ -113,7 +113,7 @@ export const columns: ColumnDef<Lead>[] = [
           <DropdownMenuTrigger asChild>
             <Button variant="ghost" className="h-8 w-8 p-0">
               <span className="sr-only">Open menu</span>
-              <DotsHorizontalIcon className="h-4 w-4" />
+              <MoreHorizontal className="h-4 w-4" />
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
@@ -173,7 +173,7 @@ export function LeadsTable({ leads }: { leads: Lead[] }) {
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="outline" className="ml-auto">
-              Columns <ChevronDownIcon className="ml-2 h-4 w-4" />
+              Columns <ChevronDown className="ml-2 h-4 w-4" />
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
